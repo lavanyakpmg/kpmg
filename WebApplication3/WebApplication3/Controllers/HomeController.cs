042332d5230c538b52a599c96b56d69cd22e191b
lavanya <lavanyaangadi@kpmg.com>
2021-10-28 18:09:58 +0530

    myfirstcommit

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WebApplication2.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+        public ActionResult Weather()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string WeatherDetail(string City)
+        {
+
+            //Assign API KEY which received from OPENWEATHERMAP.ORG  
+            string appId = "221f548ad68c81526283c7967e45811b";
+
+            //API path with CITY parameter and other parameters.  
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+
+                RootObject weatherInfo = (new JavaScriptSerializer()).Deserialize<RootObject>(json);
+
+                //Special VIEWMODEL design to send only required fields not all fields which received from   
+                //www.openweathermap.org api  
+                WeatherViewModel rslt = new WeatherViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                //Converting OBJECT to JSON String   
+                var jsonstring = new JavaScriptSerializer().Serialize(rslt);
+
+                //Return JSON string.  
+                return jsonstring;
+            }
+        }
+
+            public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
